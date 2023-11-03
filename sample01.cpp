c23bc9557bc6d0b7dc2778fb7534373687f872fa
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2023-11-03 20:30:43 +0530

    Initial Commit

---
+++ sample01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "My Name is Vikas Prajapati " << endl 
+         << "I work in MulticoreWare as a Junior Engineer." << endl;
+
+    return 0;
+}
