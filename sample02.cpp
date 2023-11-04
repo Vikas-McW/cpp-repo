8c519ec704498a15a6c5ab6329af5f7037f3ff9f
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2023-11-05 00:38:06 +0530

    Added sample02.cpp file

---
+++ sample02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    long num;
+    cout << "Enter a number : ";
+    cin >> num;
+    cout << "The Square of number is " << num * num <<" ." << endl;
+
+    return 0;
+}
