71d269459324fbb97a5090317623298a46656286
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2023-11-05 00:48:18 +0530

    added name in sample01.cpp

--- sample01.cpp
+++ sample01.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "My Name is Vikas Prajapati " << endl 
          << "I work in MulticoreWare as a Junior Engineer." << endl;
+    
+    string name;
+    cout << "Enter your name : "; 
+    cin >> name;
+    cout << "Hello " << name << endl;
 
     return 0;
 }
