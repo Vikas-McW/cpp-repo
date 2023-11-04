52dcb417899f815e0aaef404cc97541d092c3f0e
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2023-11-05 02:14:21 +0530

    Added printMessage function

--- sample01.cpp
+++ sample01.cpp
@@ -2,10 +2,21 @@
 
 using namespace std;
 
+void printMessage(void);
+
 int main(int argc, char *argv[]) {
     cout << "My Name is Vikas Prajapati " << endl 
          << "I work in MulticoreWare as a Junior Engineer." << endl;
-    
+
     cout << "Program End..." << endl;
+
+    printMessage();
+    
     return 0;
+}
+
+void printMessage(void) {
+    cout << "Welcome to the world of C++" << endl
+         << "Programmer can use this code for the testing purpose." << endl
+         << "Do not use this cpp file the project purpose. " << endl;
 }
