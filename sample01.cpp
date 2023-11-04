9047302f784a2b95c6a0c5f4a66e866c8605f9b8
Vikas-McW <vikas.prajapati@multicorewareinc.com>
2023-11-05 02:10:23 +0530

    Added new line in sample01.cpp

--- sample01.cpp
+++ sample01.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "My Name is Vikas Prajapati " << endl 
          << "I work in MulticoreWare as a Junior Engineer." << endl;
-
+    
+    cout << "Program End..." << endl;
     return 0;
 }
